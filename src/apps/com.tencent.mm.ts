70e6bc580783f452fbd50856b64432e8f9146e16
ccav14 <1044747052@qq.com>
2025-03-29 15:05:47 +0800

    Update com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -3,5 +3,21 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [],
+  groups: [
+{
+      key: 0,
+      name: '立即签到',
+      rules: [
+             {
+	activityIds: '.plugin.appbrand.ui.AppBrandUI00',
+	
+ matches: [
+            '[text="立即签到"][visibleToUser=true]',
+          ],
+             
+},
+            ],  
+},
+          ],
+
 });
