3e5b2c002d0b84f6d1431b5434f1dbd72442b2dd
ccav14 <1044747052@qq.com>
2025-03-29 16:01:22 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,22 +1,8 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
+import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
 import globalGroups from './globalGroups';
-import { RawApp, RawAppGroup } from '@gkd-kit/api';
-import { batchImportApps } from '@gkd-kit/tools';
-import { OPEN_AD_ORDER } from './globalGroups';
 
-const apps = await batchImportApps(`${import.meta.dirname}/apps`);
-const rawApps: RawApp[] = [];
-apps.forEach((appConfig) => {
-  appConfig.groups?.forEach((g: RawAppGroup) => {
-    if (!g.name.startsWith('开屏广告')) {
-      g.enable = false;
-    } else {
-      g.order = OPEN_AD_ORDER;
-    }
-  });
-  rawApps.push(appConfig);
-});
 
 export default defineGkdSubscription({
   id: 1233,
