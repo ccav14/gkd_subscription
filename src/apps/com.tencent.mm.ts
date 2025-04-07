db5a9668a8f4e8d1159a08bbd4d210fc35e90771
ccav14 <1044747052@qq.com>
2025-04-07 23:02:07 +0800

    Update com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -3,17 +3,39 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [
-    {
-      key: 0,
-      name: '立即签到',
-      rules: [
+ groups: [
         {
-          activityIds: '.plugin.appbrand.ui.AppBrandUI00',
-
-          matches: ['[text="立即签到"][visibleToUser=true]'],
+        	key: 0,
+	        name: '瑞羚厚德签到',
+	         rules: [
+            {
+              matches: '[text="立即签到"][visibleToUser=true]',
+              activityIds: ['.plugin.appbrand.ui.AppBrandUI00']
+            },
+          ],
+        },
+        {
+           key: 1,
+	         name: '白签到',
+             rules: [
+            {
+              matches: '[text="立即签到"][visibleToUser=true]',
+              activityIds: ['.plugin.webview.ui.tools.MMWebViewUI']
+            },
+          ],
+        },
+        {
+           key: 2，
+            name: '雷签到',
+            rules: [
+            {
+              matches: '[text="点击签到"][visibleToUser=true]',
+              activityIds: ['.plugin.webview.ui.tools.MMWebViewUI']
+            },
+          ],
         },
       ],
-    },
-  ],
-});
+    });
+
+            
+
