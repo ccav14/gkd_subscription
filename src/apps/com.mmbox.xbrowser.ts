9ca2aca602c218debfd5835153ad0caa019d9804
ccav14 <1044747052@qq.com>
2025-12-02 19:51:09 +0800

    Update com.mmbox.xbrowser.ts

--- src/apps/com.mmbox.xbrowser.ts
+++ src/apps/com.mmbox.xbrowser.ts
@@ -1,1 +1,19 @@
+import { defineGkdApp } from '@gkd-kit/define';
 
+export default defineGkdApp({
+  id: 'com.mmbox.xbrowser',
+  name: 'X浏览器',
+  groups: [ 
+    { 
+      key: 0,
+      name: '瑞羚签到',
+      rules: [ 
+        {
+          activityIds: ['.BrowserActivity'],
+          matches: '[text="立即签到"][visibleToUser=true]',
+        },
+      ], 
+    }, 
+  ], 
+}
+); 
