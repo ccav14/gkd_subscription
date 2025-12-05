1b0017f5dba895dd4d5cb49c28ffd2dba027761d
ccav14 <1044747052@qq.com>
2025-12-05 20:38:26 +0800

    Update com.android.browser.ts

--- src/apps/com.android.browser.ts
+++ src/apps/com.android.browser.ts
@@ -1,30 +1,27 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  id: 'com.android.browser',
-  name: '浏览器',
-  groups: [
-    {
-      key: 0,
-      name: '瑞羚签到',
-      rules: [
-        {
-         
-          activityIds: ['.BrowserActivity'],
-          matches: '[text="立即签到"][visibleToUser=true]',
-        },
-      ],
-    },
-   
+  id: 'com.android.browser',
+  name: '浏览器',
+  groups: [
     {
-      key: 1， 
+      key: 0,
+      name: '瑞羚签到',
+      rules: [
+        {
+          activityIds: ['.BrowserActivity'],
+          matches: '[text="立即签到"][visibleToUser=true]',
+        }，
+      ],
+    },
+    {
+      key: 1,
       name: '去观看',
       rules: [
         {
-         
           matches: '[text="去观看"][visibleToUser=true]',
         },
       ],
-    },
-  ],
+    }，
+  ],
 });
