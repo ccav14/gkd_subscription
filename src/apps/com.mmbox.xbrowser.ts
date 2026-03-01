6a5cbc985fc6ad26611d24cc92b47b7f45aa5af1
ccav14 <1044747052@qq.com>
2026-03-01 16:18:30 +0800

    Update com.mmbox.xbrowser.ts

--- src/apps/com.mmbox.xbrowser.ts
+++ src/apps/com.mmbox.xbrowser.ts
@@ -2,15 +2,36 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.mmbox.xbrowser',
-  name: 'X浏览器',
+   name: 'X浏览器',
   groups: [
     {
       key: 0,
       name: '瑞羚签到',
+      enable: false,
       rules: [
         {
-          activityIds: ['.BrowserActivity'],
           matches: '[text="立即签到"][visibleToUser=true]',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '瑞羚签到2',
+      rules: [
+        {
+          matches:
+            '[text$="立即签到"][name="android.widget.TextView"][visibleToUser=true]',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '进入直播',
+      rules: [
+        {
+          activityIds: ['com.mmbox.xbrowser.pro.BrowserActivity'],
+          matches:
+            '[text="进入直播"][name="android.widget.TextView"][visibleToUser=true]',
         },
       ],
     },
