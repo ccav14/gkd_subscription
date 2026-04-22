95e4e95cac6cfa72da0a892b0b0ba1795025ea78
ccav14 <1044747052@qq.com>
2026-04-22 20:17:25 +0800

    Update com.chinamobile.mcloud.ts

--- src/apps/com.chinamobile.mcloud.ts
+++ src/apps/com.chinamobile.mcloud.ts
@@ -9,9 +9,7 @@ export default defineGkdApp({
       name: '自动签到',
       desc: '自动点击继续赚云朵-领云朵-知道啦',
       enable: true,
-      activityIds:
-        'com.chinamobile.mcloud.client.component.web.H5WebviewMainActivity',
-      rules: [
+          rules: [
         {
           key: 0,
           name: '1.点击【继续赚云朵】',
